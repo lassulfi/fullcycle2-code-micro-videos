ff6b9b2073d272fb8a8e12ee04e71e67477e715f
Luis Daniel Assulfi <lassulfi@gmail.com>
2021-04-26 23:05:33 +0000

    Testes com HTTP

--- app/Http/Controllers/Api/GenreController.php
+++ app/Http/Controllers/Api/GenreController.php
@@ -32,7 +32,9 @@ class GenreController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, $this->rules);
-        return Genre::create($request->all());
+        $genre = Genre::create($request->all());
+        $genre->refresh();
+        return $genre;
     }
 
     /**
