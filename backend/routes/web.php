72f2872116bfc1c00c6d454c2216b9f0d5f1cb25
Luis Daniel Assulfi <lassulfi@gmail.com>
2021-10-26 22:09:59 -0300

    Criando testes no browser para verificar o funcionamento do React.js

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/{react?}', function($react = null) {
+    return view('admin-frontend.index');
+})->where('react', '.*');
