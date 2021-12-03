6d785bc14309dc899e90f2012d54478695a85f72
Luis Daniel Assulfi <lassulfi@gmail.com>
2021-12-02 22:08:54 -0300

    Utilizando o serviço de validação nas fixtures e na sincronização

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\ModelFilters\CategoryFilter;
+use App\Models\Traits\SerializeDateToIso8601;
 use App\Models\Traits\Uuid;
 use EloquentFilter\Filterable;
 use Illuminate\Database\Eloquent\Model;
@@ -10,7 +11,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use SoftDeletes, Uuid, Filterable;
+    use SoftDeletes, Uuid, Filterable, SerializeDateToIso8601;
 
     protected $fillable = ['name', 'description', 'is_active'];
     protected $dates = ['deleted_at'];
